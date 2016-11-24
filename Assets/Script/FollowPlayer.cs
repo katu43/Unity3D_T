b483d0a37b247a0dfa5f16c01ebbfce02d263407
katu432000 <katu432007@yahoo.co.jp>
2016-11-24 21:26:58 +0900

    Item_Add
    Item追加まで

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform target;
+	private Vector3 offset;
+
+	void Start(){
+		// Playerとの相対距離を求める
+		offset = GetComponent<Transform> ().position - target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 自分の座標にPlayerの相対距離を足した値を設定する
+		GetComponent<Transform>().position = target.position + offset;
+
+	}
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float Speed;
+	
+	void FixedUpdate(){
+
+		// 入力をxとzに代入
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+
+		// 同一のGameObjectが持つRigidBodyを取得する。
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+		// rigidBodyにx軸とz軸に力を加える。
+		rigidbody.AddForce(x * Speed, 0, z * Speed);
+	}
+}
